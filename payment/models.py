35f29bfb59d823cadcb85d194bc83eeee4db5e67
tonytettinger <antal.tettinger@gmail.com>
2020-09-29 07:48:44 +0200

    Corrected file locations

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+		amount = models.DecimalField(max_digits=5, decimal_places=2)
+		time = models.CharField(max_length=200)
+		orderid = models.CharField(max_length=255)
+		quote = models.CharField(max_length=255)
+
+		def __str__(self):
+			return self.time
